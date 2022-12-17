782666b006630f1f9c1fa56f0d7877f672c43de4
VenkateswarluPola123 <106611061+VenkateswarluPola123@users.noreply.github.com>
2022-12-17 09:38:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
-  }
-}
-
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
 
