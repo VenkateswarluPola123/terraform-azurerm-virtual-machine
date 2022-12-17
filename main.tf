74b12621cc724f70639fae8cc5c182df277ba06d
VenkateswarluPola123 <106611061+VenkateswarluPola123@users.noreply.github.com>
2022-12-17 09:45:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+  features {}
+  skip_provider_registration = true
+}
+
 resource "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
 
