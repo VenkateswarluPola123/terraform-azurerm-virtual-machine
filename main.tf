a8ecad77afd19df3388d3de74b3a1deec5d4c5c9
VenkateswarluPola123 <106611061+VenkateswarluPola123@users.noreply.github.com>
2022-12-17 10:56:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,3 +32,12 @@ data "archive_file" "zip_the_python_code" {
   output_path = "${path.module}/python/hello-python.zip"
 }
 
+resource "aws_lambda_function" "terraform_lambda_func" {
+  filename                       = "${path.module}/python/hello-python.zip"
+  function_name                  = "Helloworld_Test_Lambda_Function"
+  role                           = data.aws_iam_role.iam_for_lambda.arn
+  handler                        = "index.lambda_handler"
+  runtime                        = "python3.8"
+  depends_on                     = [aws_iam_role_policy_attachment.attach_iam_policy_to_iam_role]
+}
+
