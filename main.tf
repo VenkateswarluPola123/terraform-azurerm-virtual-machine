d91075b31c3aa71318ebd5ad5059bfcd89c094cd
VenkateswarluPola123 <106611061+VenkateswarluPola123@users.noreply.github.com>
2022-12-17 10:43:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ data "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
 }
 
-resource "aws_iam_policy" "iam_policy_for_lambda" {
+data "aws_iam_policy" "iam_policy_for_lambda" {
  
   name         = "aws_iam_policy_for_terraform_aws_lambda_role"
 }
