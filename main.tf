fc42183f2fee7ac90abdfc4e1ad5dfc7015e37d3
VenkateswarluPola123 <106611061+VenkateswarluPola123@users.noreply.github.com>
2022-12-17 10:41:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,51 +12,17 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_iam_role" "iam_for_lambda" {
+data "aws_iam_role" "iam_for_lambda" {
   name = "iam_for_lambda"
-
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "lambda.amazonaws.com"
-      },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
 }
 
 resource "aws_iam_policy" "iam_policy_for_lambda" {
  
   name         = "aws_iam_policy_for_terraform_aws_lambda_role"
-  path         = "/"
-  description  = "AWS IAM Policy for managing aws lambda role"
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "logs:CreateLogGroup",
-        "logs:CreateLogStream",
-        "logs:PutLogEvents"
-      ],  
-      "Resource": "arn:aws:logs:*:*:*",
-      "Effect": "Allow"
-    }
-  ]
-}
-EOF
 }
 
 resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
- role        = aws_iam_role.iam_for_lambda.name
- policy_arn  = aws_iam_policy.iam_policy_for_lambda.arn
+ role        = data.aws_iam_role.iam_for_lambda.name
+ policy_arn  = data.aws_iam_policy.iam_policy_for_lambda.arn
 }
 
