09847662f10ea74722e54a73232911aacdc53b12
VenkateswarluPola123 <106611061+VenkateswarluPola123@users.noreply.github.com>
2022-12-20 11:36:49 +0530

    Update index.py

--- python/index.py
+++ python/index.py
@@ -1,5 +1,5 @@
 def lambda_handler(event, context):
-   message = 'Hello World {} !'.format(event['key1'])
+   message = 'Hello From Lambda {} !'.format(event['key1'])
    return {
        'message' : message
    }
